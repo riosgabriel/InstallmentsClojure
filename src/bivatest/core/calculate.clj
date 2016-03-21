b4e2cbf63a9dac84eaeadf551a1ec162dc6aab1e
Gabriel Rios <gabrielfrios@gmail.com>
2016-03-21 12:16:04 -0300

    fix divide by zero calculate

--- src/bivatest/core/calculate.clj
+++ src/bivatest/core/calculate.clj
@@ -3,8 +3,10 @@
 (defn ^:dynamic ** [x n] (reduce * (repeat n x)))
 
 (defn calculatePrice [pv i n]
-  (/
-    (* pv i)
-    (- 1
-       (/ 1
-          (** (+ 1 i) n)))))
+  (if (or (<= i 0) (<= n 0))
+     0
+     (/
+       (* pv i)
+       (- 1
+          (/ 1
+             (** (+ 1 i) n))))))
--- test/bivatest/core/calculate_test.clj
+++ test/bivatest/core/calculate_test.clj
@@ -3,4 +3,12 @@
             [bivatest.core.calculate :refer :all]))
 
 (deftest calc-price
-  (is (= 355.94375681841166 (calculatePrice 2500.0 0.07 10))))
+  (testing "calculate price"
+    (is (= 355.94375681841166 (calculatePrice 2500.0 0.07 10))))
+
+  (testing "calculate with zero number_of_installments")
+    (is (= 0 (calculatePrice 2500.0 4 0)))
+
+  (testing "calculate with zero monthly_interest_rate")
+    (is (= 0 (calculatePrice 2500.0 0 10)))
+  )
